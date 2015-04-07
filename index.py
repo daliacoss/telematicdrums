a062c0a05a6c80942f0484efd12aae5d9e3e5f23
david <cosstropolis@gmail.com>
2015-04-07 19:50:09 -0400

    trying out restful stuff

--- index.py
+++ index.py
@@ -1,20 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_restful import Api, Resource
 from flask.ext.socketio import SocketIO
 import json
 
 app = Flask(__name__)
+api = Api(app)
 socketIO = SocketIO(app)
-app.debug = True
+
+sequencerData = {
+	"sequence":[
+		[1,0,1,0,1,1,1,0],
+		[0,0,1,0,0,0,1,0],
+		[1,1,1,1,1,1,1,1],
+		[0,0,1,0,1,1,0,0],
+	],
+	"tempo":100,
+}
+
+class HelloWorld(Resource):
+
+	def get(self):
+
+		return sequencerData
+
+	def post(self):
+
+		sequencerData["sequence"][0][0] += 1
+		return "ok"
+
+api.add_resource(HelloWorld, '/trigger', methods=["GET", "POST"])
 
 @app.route("/")
 def index():
-    return render_template('index.html',)
+    return "hello"#render_template('index.html',)
 
-@socketIO.on('my event')
-def handle_source(json_data):
-    text = json_data["message"]#['message'].encode('ascii', 'ignore')
-    socketIO.emit('echo', {'echo': 'Server Says: '+text+'!'}, True)
-    print text
+# @socketIO.on('my event')
+# def handle_source(json_data):
+#     text = json_data["message"]#['message'].encode('ascii', 'ignore')
+#     socketIO.emit('echo', {'echo': 'Server Says: '+text+'!'}, True)
+#     print text
 
 if __name__ == "__main__":
-    socketIO.run(app)
+    app.run()
